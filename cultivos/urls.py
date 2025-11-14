ff4237bfe5b2ed4ae0329af4e892d515ed7c83a1
BENJAMIN IGNACIO TORRES PEREZ <benjamin.torres40@inacapmail.cl>
2025-11-14 15:00:03 -0300

    Integracion de Api

--- cultivos/urls.py
+++ cultivos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import listar_cultivos, detalle_cultivo, registrar_cultivo
+from .api_views import api_lista_cultivos
 
 urlpatterns = [
     path('', listar_cultivos, name='listar_cultivos'),
     path('agregar/', registrar_cultivo, name='registrar_cultivo'),
     path('detalle/<str:nombre>/', detalle_cultivo, name='detalle_cultivo'),
+    path('api/cultivos/', api_lista_cultivos, name='api_lista_cultivos')
 ]
